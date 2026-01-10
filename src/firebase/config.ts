b53840aa5faf38fce6ccd76e0d8063a9adb366d0
Sistemas UAI <sistemas@uai.edu.pe>
2026-01-10 19:27:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6817342158-d2380",
+  "appId": "1:1031276873260:web:f2adc15d5e9549accd7389",
+  "apiKey": "AIzaSyAPh65wzYM1xhk5jqnkvy6C975cZK1cuD0",
+  "authDomain": "studio-6817342158-d2380.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1031276873260"
+};

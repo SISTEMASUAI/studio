07067a4336f434e4cfafac9a420af3ea19454b6e
Sistemas UAI <sistemas@uai.edu.pe>
2026-02-11 16:16:10 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:1031276873260:web:f2adc15d5e9549accd7389",
   "apiKey": "AIzaSyAPh65wzYM1xhk5jqnkvy6C975cZK1cuD0",
   "authDomain": "studio-6817342158-d2380.firebaseapp.com",
+  "storageBucket": "studio-6817342158-d2380.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "1031276873260"
 };
